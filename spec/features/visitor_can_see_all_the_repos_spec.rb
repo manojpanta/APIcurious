f1930262fc7fee0812427bcf75178007b977928a
manojpanta <manojpanta95@gmail.com>
2018-07-05 17:31:52 -0600

    added test for repositories , foloowing and followers

--- spec/features/visitor_can_see_all_the_repos_spec.rb
+++ spec/features/visitor_can_see_all_the_repos_spec.rb
@@ -1,8 +1,27 @@
 require 'rails_helper'
-feature 'visitor visiting root path' do
-  scenario 'sign ins with github credentials can log in' do
-    visit root_path
-    
 
+describe 'user visits user dashboard' do
+  it 'sees starred repos' do
+    WebMock.allow_net_connect!
+    user = User.create(uid: '1234567', user_name: 'manoj', name: 'manoj', avatar_url: 'https://www.maxhealthcare.in/sites/default/files/doctors/doctor_photo/2018/May/15/Dr%20Manoj%20Kumar.jpg', oauth_token: ENV["api_curious_token"])
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    url = "https://api.github.com/users/#{user.user_name}/repos?access_token=#{user.oauth_token}"
+
+    response = File.read('./spec/fixtures/repos.json')
+
+    stub_request(:get, url).to_return(status: 200, body: response)
+
+    visit "/"
+
+    expect(page).to have_content(user.user_name)
+    visit "/repositories"
+    expect(page).to have_css('.repo_name', count: 30)
+    visit "/following"
+    expect(page).to have_css('.repo_name', count: 6)
+    visit "/followers"
+    expect(page).to have_css('.repo_name', count: 2)
+
+    WebMock.disable_net_connect!
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,21 +1,7 @@
-OmniAuth.config.test_mode = true
-omniauth_hash = { 'provider' => 'github',
-                  'uid' => '12345',
-                  'info' => {
-                      'name' => 'manoj',
-                      'email' => 'manojpanta.yahoo.com',
-                      'nickname' => 'manoj'
-                  },
-                  'extra' => {'raw_info' =>
-                                  { 'location' => 'San Francisco',
-                                    'gravatar_id' => '123456789'
-                                  }
-                  }
-}
 
-OmniAuth.config.add_mock(:github, omniauth_hash)
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
+
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
 
