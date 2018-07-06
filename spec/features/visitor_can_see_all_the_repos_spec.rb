33d5b7d1c496b334caf64762c6efc62dad9ecf50
manojpanta <manojpanta95@gmail.com>
2018-07-05 21:47:03 -0600

    still one failing test but done for today

--- spec/features/visitor_can_see_all_the_repos_spec.rb
+++ spec/features/visitor_can_see_all_the_repos_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 describe 'user visits user dashboard' do
   it 'sees starred repos' do
+    WebMock.allow_net_connect!
     user = User.create(uid: '1234567', user_name: 'manoj', name: 'manoj', avatar_url: 'https://www.maxhealthcare.in/sites/default/files/doctors/doctor_photo/2018/May/15/Dr%20Manoj%20Kumar.jpg', oauth_token: ENV["api_curious_token"])
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
@@ -21,5 +22,6 @@ describe 'user visits user dashboard' do
     visit "/followers"
     expect(page).to have_css('.repo_name', count: 2)
 
+    WebMock.disable_net_connect!
   end
 end
